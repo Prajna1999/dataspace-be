92d351240d210cccaaceb84c04d2e0fb59b095a1
Prajna1999 <gituprajna20@gmail.com>
2024-09-29 21:27:22 +0530

    ADD: healthcheck endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
-    "fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+// define JSON object type
+type HealthCheckResponse struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+	router := gin.Default()
+
+	// group the versioned API
+	v1 := router.Group("/api/v1")
+
+	{
+		v1.GET("/health-check", healthCheck)
+	}
+
+	router.Run(":8080")
+}
+
+func healthCheck(c *gin.Context) {
+	response := HealthCheckResponse{
+		Status:  "OK",
+		Message: "All Systems Narmal",
+	}
+	c.JSON(http.StatusOK, response)
 }
